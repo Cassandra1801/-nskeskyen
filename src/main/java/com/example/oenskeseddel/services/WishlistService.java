09119123272f358a9292434711d64ad174813fd0
BjarkeHolmgaard <b.holmgaard2001@gmail.com>
2026-04-14 13:09:10 +0200

    Added WishlistController class and edited WishListService accordingly

--- src/main/java/com/example/oenskeseddel/services/WishlistService.java
+++ src/main/java/com/example/oenskeseddel/services/WishlistService.java
@@ -2,6 +2,7 @@ package com.example.oenskeseddel.services;
 
 import com.example.oenskeseddel.models.User;
 import com.example.oenskeseddel.models.Wishlist;
+import com.example.oenskeseddel.repositories.UserRepository;
 import com.example.oenskeseddel.repositories.WishlistRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,17 @@ import org.springframework.stereotype.Service;
 public class WishlistService {
 
     private final WishlistRepository wishlistRepository;
+    private final UserRepository userRepository;
 
-    public WishlistService(WishlistRepository wishlistRepository) {
+    public WishlistService(WishlistRepository wishlistRepository, UserRepository userRepository) {
         this.wishlistRepository = wishlistRepository;
+        this.userRepository = userRepository;
     }
 
-    public Wishlist createWishlist(User user, String name) {
+    public Wishlist createWishlist(Integer userId, String name) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Bruger ikke fundet"));
+
         return wishlistRepository.save(new Wishlist(user, name));
     }
 }
