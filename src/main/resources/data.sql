4840b4baa7247b7dba758ffc0856e4c59223fb71
AquaticBlue <tokiarb@gmail.com>
2026-04-15 09:50:13 +0200

    Debugging

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO Users (username, password, mail, name)
+INSERT INTO users (username, password, email, name)
 SELECT 'testuser', '1234', 'testuser@mail.com', 'Test User'
     WHERE NOT EXISTS (
     SELECT 1 FROM Users WHERE username = 'testuser'
