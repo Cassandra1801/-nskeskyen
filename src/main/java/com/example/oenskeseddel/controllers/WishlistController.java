4a377c70c8d7c9800497d51119a5f242a7d5d70e
Cassandra1801 <cassandramichaelsen@gmail.com>
2026-04-16 10:56:00 +0200

    front-end

--- src/main/java/com/example/oenskeseddel/controllers/WishlistController.java
+++ src/main/java/com/example/oenskeseddel/controllers/WishlistController.java
@@ -2,6 +2,7 @@ package com.example.oenskeseddel.controllers;
 
 import com.example.oenskeseddel.models.Wishlist;
 import com.example.oenskeseddel.services.WishlistService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,5 +19,14 @@ public class WishlistController {
     public Wishlist createWishlist(@RequestParam Integer userId,
                                    @RequestParam String name) {
         return wishlistService.createWishlist(userId, name);
+    }
+
+    
+    public class PageController {
+
+        @GetMapping("/opret")
+        public String showCreateWishlistPage() {
+            return "opret";
+        }
     }
 }
