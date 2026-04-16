13650fb61ff55959626d5d8231da7931a9ed9135
MariamGh2 <magh0004@stud.ek.dk>
2026-04-16 20:37:34 +0200

    Nu kan man oprette en ønskeliste

--- src/main/java/com/example/oenskeseddel/controllers/PageController.java
+++ src/main/java/com/example/oenskeseddel/controllers/PageController.java
@@ -1,12 +1,31 @@
 package com.example.oenskeseddel.controllers;
 
+import com.example.oenskeseddel.services.WishlistService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
-    @GetMapping("/opret")
+
+    private final WishlistService wishlistService;
+
+    public PageController(WishlistService wishlistService) {
+        this.wishlistService = wishlistService;
+    }
+
+    @GetMapping("/createWishlist")
     public String showCreateWishlistPage() {
         return "opret";
+    }
+
+    @PostMapping("/createWishlist")
+    public String createWishlist(@RequestParam Integer userId,
+                                 @RequestParam String name) {
+
+        wishlistService.createWishlist(userId, name);
+
+        return "redirect:/";
     }
 }
